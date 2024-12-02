d7172029000d4e376e0514b7c14910e6e31d9872
nazarenkovv <vitanazarenko2006@yandex.ru>
2024-12-02 22:06:20 +0500

    Task6

---
+++ Tyuiu.NazarenkoVV.Sprint4.Task6.V21/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.NazarenkoVV.Sprint4.Task6.V21.Lib;
+
+namespace Tyuiu.NazarenkoVV.Sprint4.Task6.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Назаренко В. В. | ИСПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (ввод с клавиатуры)                            *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Назаренко В. В.             | ИСПб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан строковый массив данных                                             *");
+            Console.WriteLine("* используя класс Array подсчитайте количество элементов, длина которых   *");
+            Console.WriteLine("*  меньше 8                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            var week = new string[] { "Facebook", "Twitter", "Instagram", "Snapchat", "LinkedIn", "Pinterest", "Reddit" };
+
+            Console.WriteLine("Исходный массив");
+            for (int i = 0; i <= week.Length - 1; i++)
+            {
+                Console.WriteLine(week[i]);
+            }
+
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* Количество элементов, длина которых меньше 8:                              *");
+
+            int nums = ds.Calculate(week);
+
+            Console.WriteLine(nums);
+            Console.ReadKey();
+        }
+    }
+}
